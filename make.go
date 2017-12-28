a53966e3ee7fc3aef8ae6bd3da077afb27842b85
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-12-28 15:52:20 +1100

    Moved make out, go's package scoping doesnt seem to work with my brain

---
+++ make.go
@@ -0,0 +1,35 @@
+package kita
+
+import (
+	"fmt"
+	"github.com/kirillrdy/kita/shell"
+	"runtime"
+)
+
+func Make(source PackageSource, env []string) {
+	what := source.BuildPath()
+
+	confiure(source, env)
+
+	makeBin := "make"
+	if runtime.GOOS == "freebsd" {
+		makeBin = "gmake"
+	}
+
+	shell.ExecDirEnv(what, makeBin, []string{}, env)
+	shell.ExecDirEnv(what, makeBin, []string{"install"}, env)
+}
+
+func confiure(source PackageSource, env []string) {
+	what := source.BuildPath()
+	toWhere := source.PackageVersion.WorldPath()
+
+	prefixArg := fmt.Sprintf("--prefix=%s", toWhere)
+	args := []string{"configure", prefixArg}
+
+	if source.PackageVersion.Package.Name == "gcc" {
+		args = append(args, "--disable-multilib")
+	}
+
+	shell.ExecDirEnv(what, "sh", args, env)
+}
