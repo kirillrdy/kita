7efebfb8c363cac5265ef14ddd01eef0a6b77e0a
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-12-28 15:52:38 +1100

    Exec with env overwrite

--- shell/exec.go
+++ shell/exec.go
@@ -2,20 +2,31 @@ package shell
 
 import (
 	"log"
+	"os"
 	"os/exec"
 )
 
-func ExecDir(dir string, cmdName string, args ...string) {
-	log.Printf("%% %v %v", cmdName, args)
+const verboseBuilding = true
+
+func ExecDirEnv(dir string, cmdName string, args []string, env []string) {
+	log.Printf("%% %v %v %v", cmdName, args, env)
 	cmd := exec.Command(cmdName, args...)
 	cmd.Dir = dir
+	cmd.Env = append(os.Environ(), env...)
+	if verboseBuilding {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
 	err := cmd.Run()
 	//TODO something better than crash here
 	if err != nil {
 		log.Panic(err)
 	}
 }
+func ExecDir(dir string, cmdName string, args []string) {
+	ExecDirEnv(dir, cmdName, args, []string{})
+}
 
 func Exec(cmdName string, args ...string) {
-	ExecDir("", cmdName, args...)
+	ExecDir("", cmdName, args)
 }
