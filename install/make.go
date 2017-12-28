f95966d29fb8906fbb407482703e24d706c26381
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-12-28 13:14:46 +1100

    Extracted configure

--- install/make.go
+++ install/make.go
@@ -8,14 +8,19 @@ import (
 
 func Make(what string, toWhere string) {
 
-	prefixArg := fmt.Sprintf("--prefix=%s", toWhere)
+	confiure(what, toWhere)
 
 	makeBin := "make"
 	if runtime.GOOS == "freebsd" {
 		makeBin = "gmake"
 	}
 
-	shell.ExecDir(what, "sh", "configure", prefixArg)
 	shell.ExecDir(what, makeBin)
 	shell.ExecDir(what, makeBin, "install")
+}
+
+func confiure(what string, toWhere string) {
+	prefixArg := fmt.Sprintf("--prefix=%s", toWhere)
+	args := []string{"configure", prefixArg}
+	shell.ExecDir(what, "sh", args...)
 }
